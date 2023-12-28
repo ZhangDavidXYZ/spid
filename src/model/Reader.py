54396761b2a45962cf3435fb36bb39e7d8af4b8a
ZhangDavidXYZ <Z.David.0318@gmail.com>
2023-12-28 15:42:59 +0800

    Update Reader.py

--- src/model/Reader.py
+++ src/model/Reader.py
@@ -1,6 +1,9 @@
-
 class Reader:
-    def __init__(self):
-        pass
+    def __init__(self, file_name, path):
+        self.__path = path + file_name
+        self.__file_name = file_name
 
-    def read_file(self):
+    def read(self):
+        with open(self.__path, mode="r") as f:
+            return f.read()
+
